90abb90a69ad42ebb3bab2202cb31eeadd5d03b2
Animesh Shrivastava <ani979@gmail.com>
2018-08-08 14:30:48 +0530

    New changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './address-card/user.model';
+import {TestService} from './test.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-project';
+  user:User
+  inputText:String = "Hello"
+  title = 'first-project app';
+
+  constructor(private svc:TestService) {
+    this.user = new User();
+    this.user.name="Animesh";
+    this.user.designation =" Into software";
+    this.user.address="PFR, Blr";
+    this.user.phone=['123-45-689', '987-043-235','122-03230-22', '222we-eer--po'];
+    this.svc.callMe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { DateComponent } from './date/date.component';
+import { AddressCardComponent } from './address-card/address-card.component';
+import { ViewModule } from './view/view.module'
+import { TestService } from './test.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    DateComponent,
+    AddressCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ViewModule
   ],
-  providers: [],
+  providers: [TestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
